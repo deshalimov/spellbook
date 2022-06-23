e6ce91171bfd8d4773e3156dda7b77fcde1fdfe0
Denis Shalimov <denis.shalimov98@gmail.com>
2022-06-23 20:28:22 +0300

    Add filtration

--- test/spellbook/schools/queries/schools_by_name_test.exs
+++ test/spellbook/schools/queries/schools_by_name_test.exs
@@ -3,6 +3,7 @@ defmodule Spellbook.Schools.Queries.SchoolByNameTest do
 
   alias Spellbook.Schools
 
+  @tag :filterschool
   test "process/1 get_school_by_name test" do
     sch = insert(:school)
     result = Spellbook.Schools.Queries.SchoolByName.process(%{nameSchool: sch.nameSchool})
